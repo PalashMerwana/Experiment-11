71a3f495bf24e426e701fcd1111baab0a38b97a3
PalashMerwana <palash.merwana.btech2023@sitpune.edu.in>
2024-09-15 08:40:57 +0530

    Add files via upload

---
+++ classes_and_objects_3.cpp
@@ -0,0 +1,28 @@
+//Palash Merwana 
+//Prn: 23070123095
+//experiment 11: classes and objects
+
+#include <iostream>
+using namespace std;
+
+class cube
+{
+    private:
+    int height=2;
+    int width=3;
+    int length=5;
+
+    public:
+    int volume()
+    {
+        double v;
+        v=height*width*length;
+        return v;
+    }
+};
+
+int main()
+{
+    cube cube1;
+    cout<<"volume: "<<cube1.volume()<<endl;
+}
